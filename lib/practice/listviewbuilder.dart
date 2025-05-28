1edff0b625c41b939b42e98c42fd5561c71d091a
Hemanthmuvvala <hemanth.m2111@gmail.com>
2025-05-28 14:46:02 +0530

    gridview builder

--- lib/practice/listviewbuilder.dart
+++ lib/practice/listviewbuilder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Listviewbuilder extends StatefulWidget {
   const Listviewbuilder({super.key});
 
@@ -8,24 +9,78 @@ class Listviewbuilder extends StatefulWidget {
 }
 
 class _ListviewbuilderState extends State<Listviewbuilder> {
+  List<String> names = ['hello', 'world', 'java'];
+  List<String> imagePaths = [
+    'assets/images/emoji.png',
+    'assets/images/games.png',
+    'assets/images/play.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-        return  myContainer(index);
-        },
-      ),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('app'),
+          leading: InkWell(
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        height:200,
+                        color:Colors.yellow,
+                        child: Column(
+                          children: [
+                            ListTile(title: Text('jai ganesh')),
+                            ListTile(title: Text('jai ganesh')),
+                             ListTile(title: Text('jai ganesh')),
+                            IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: Icon(Icons.arrow_downward))
+                          ],
+                        ),
+                      );
+                    });
+              },
+              child: Icon(Icons.menu)),
+        ),
+        // body: GridView.builder(
+        //     // physics:BouncingScrollPhysics(),
+        //     itemCount: 30,
+        //     gridDelegate:
+        //         SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        //     itemBuilder: (ctx, index) {
+        //       return InkWell(
+        //         onTap: () {
+        //           Navigator.push(context,
+        //               MaterialPageRoute(builder: (ctx) => Mailhomescreen()));
+        //         },
+        //         child: Card(
+        //           color: Colors.blue,
+        //           elevation: 10,
+        //           child: Column(
+        //             mainAxisAlignment: MainAxisAlignment.center,
+        //             children: [
+        //               Text('jai babu'),
+        //               Icon(Icons.flag),
+        //             ],
+        //           ),
+        //         ),
+        //       );
+        //     })
+        // 
+        );
   }
 
-  Widget myContainer(int index) {
-    return Container(
-      height: 200,
-      width: 200,
-      color: Colors.blue,
-      child: Text("$index"),
-    );
-  }
+  // Widget myContainer(int index) {
+  //   return Container(
+  //     height: 200,
+  //     width: 200,
+  //     color: Colors.blue,
+  //     child: Text("$index"),
+  //   );
+  // }
 }
